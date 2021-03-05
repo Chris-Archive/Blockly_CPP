f44be55eb5a933282415853e0cc9bb19e6b9f8e4
Chris <>
2021-03-05 00:48:21 -0500

    Initial Commit

---
+++ demos/code/Blocks/Core.js
@@ -0,0 +1,27 @@
+
+Blockly.Blocks["main"] = {
+	init: function(){
+		
+		this.appendDummyInput()
+			.appendField("int main()");
+
+		this.appendStatementInput("statement");
+
+
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+	}
+};
+
+Blockly.C["main"] = function(){
+	var s_input = Blockly.C.statementToCode(this, "statement");
+	var code = "";
+	
+	code += "int main(){\n";
+
+	code += s_input;
+
+	code += "}";
+
+	return code;
+};
